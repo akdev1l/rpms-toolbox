akdev1l/rpms-toolbox

commit 86f63f79648cbd7f05b807d458b6c02c54774df3

Update to 0.0.10

toolbox.spec
@@ -1,5 +1,5 @@
 Name:          toolbox
-Version:       0.0.9
+Version:       0.0.10
 Release:       1%{?dist}
 Summary:       Unprivileged development environment
 
@@ -17,7 +17,6 @@ BuildRequires: meson
 BuildRequires: pkgconfig(bash-completion)
 BuildRequires: systemd
 
-Requires:      buildah
 Requires:      podman
 
 # To be removed in Fedora 33
@@ -130,6 +129,9 @@ Dockerfile if the image isn't based on the fedora-toolbox image.
 
 
 %changelog
+* Tue May 21 2019 Debarshi Ray <rishi@fedoraproject.org> - 0.0.10-1
+- Update to 0.0.10
+
 * Tue Apr 30 2019 Debarshi Ray <rishi@fedoraproject.org> - 0.0.9-1
 - Update to 0.0.9
 

.gitignore
@@ -2,3 +2,4 @@
 /toolbox-0.0.7.tar.xz
 /toolbox-0.0.8.tar.xz
 /toolbox-0.0.9.tar.xz
+/toolbox-0.0.10.tar.xz

sources
@@ -1,1 +1,1 @@
-SHA512 (toolbox-0.0.9.tar.xz) = f59bf470bb5f5229b7f4dbd6f74093bac7a58eaeb3dbb2f20969b137d99f8e267d228c99aac5fca899a14e64636816852d3049c1ae7a6fb69f709b47a3ff5193
+SHA512 (toolbox-0.0.10.tar.xz) = 5a0aa60ce9c93c5467b866458a8af51a6d729346f0333d66b597160b8a62c1aef679be59ad509e6e4555587d816bf05f8249708493c62cb9781c145c8f5f6dc0

commit 3e708619ba5d16be1940b99036a5b6d28d14be9d

Toolbox no longer uses Buildah
No need to issue a build just for this.

Fallout from 86f63f79648cbd7f05b807d458b6c02c54774df3

toolbox.spec
@@ -8,7 +8,7 @@ URL:           https://github.com/containers/toolbox
 Source0:       https://github.com/containers/%{name}/releases/download/%{version}/%{name}-%{version}.tar.xz
 
 BuildArch:     noarch
-# buildah and podman only work on the following architectures:
+# podman only works on the following architectures:
 ExclusiveArch: aarch64 %{arm} ppc64le s390x x86_64
 
 BuildRequires: ShellCheck