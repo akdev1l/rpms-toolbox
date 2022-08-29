91cf2552ed9011a781c66482d25fe8aac9270b6d
Alex Diaz <alex@akdev.xyz>
2022-08-28 23:10:57 -0400

    fix: accept install of builddep

--- ci/build.sh
+++ ci/build.sh
@@ -4,5 +4,5 @@ version="$1"
 curl -L "https://github.com/akdev1l/toolbox/archive/refs/tags/${version}.tar.gz" \
   -o "$HOME/rpmbuild/SOURCES/${version}.tar.gz"
 
-sudo dnf builddep --spec toolbox.spec
+sudo dnf builddep -y --spec toolbox.spec
 rpmbuild -ba toolbox.spec
