212b5d382ad9b2406c78ba4abf05520a7e5154f4
Alex Diaz <alex@akdev.xyz>
2022-08-28 23:13:42 -0400

    fix: create SOURCES dir before downloading release tarball

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 version="$1"
+
+mkdir -p "$HOME/rpmbuild/SOURCES"
 curl -L "https://github.com/akdev1l/toolbox/archive/refs/tags/${version}.tar.gz" \
   -o "$HOME/rpmbuild/SOURCES/${version}.tar.gz"
 
